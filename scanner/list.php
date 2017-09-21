1ca58244ef682ea88747078eadd53b5775333883
Rindula <rindola@t-online.de>
2017-09-21 16:39:35 +0200

    Bugfix

--- scanner/list.php
+++ scanner/list.php
@@ -17,6 +17,7 @@
                         }
                         setTimeout(function() {
                             update(document.getElementById("datepick").value);
+                            old = "";
                         }, 1000);
                     }
                     if (this.readyState == 4 && this.status != 200) {
