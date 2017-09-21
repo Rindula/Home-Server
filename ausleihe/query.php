3f59e6c9eb214319c77a921406ac14d04e6b60b1
Rindula <rindola@t-online.de>
2017-09-21 19:37:12 +0200

    Debug

--- ausleihe/query.php
+++ ausleihe/query.php
@@ -5,9 +5,12 @@ $conn = new mysqli("localhost", "root", "SiSal2002", "etiketten");
 
 if ($t == "add") {
     $conn->query("INSERT INTO zeug (bez) VALUES ('".$conn->escape_string($_POST["bez"])."')");
+    echo $conn->error . PHP.EOL;
     $r = $conn->query("SELECT max(id) FROM zeug");
+    echo $conn->error . PHP.EOL;
     $id = $r->fetch_assoc["id"];
     $conn->query("INSERT INTO beschreibung (id, code, konsole) VALUES ($id, '".$conn->escape_string($_POST["code"])."', ".$conn->escape_string($_POST["konsole"]).");");
+    echo $conn->error . PHP.EOL;
 }
 
 if($t == "get") {
