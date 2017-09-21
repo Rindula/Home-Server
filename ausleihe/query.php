c239742517ab0ec8134356c0836727f82148d26a
Rindula <rindola@t-online.de>
2017-09-21 19:38:50 +0200

    Fix

--- ausleihe/query.php
+++ ausleihe/query.php
@@ -5,12 +5,9 @@ $conn = new mysqli("localhost", "root", "SiSal2002", "etiketten");
 
 if ($t == "add") {
     $conn->query("INSERT INTO zeug (bez) VALUES ('".$conn->escape_string($_POST["bez"])."')");
-    echo $conn->error . PHP.EOL;
     $r = $conn->query("SELECT max(id) FROM zeug");
-    echo $conn->error . PHP.EOL;
     $id = $r->fetch_assoc["id"];
-    $conn->query("INSERT INTO beschreibung (id, code, konsole) VALUES ($id, '".$conn->escape_string($_POST["code"])."', ".$conn->escape_string($_POST["konsole"]).");");
-    echo $conn->error . PHP.EOL;
+    $conn->query("INSERT INTO beschreibung (id, code, konsole) VALUES ($id, '".$conn->escape_string($_POST["code"])."', '".$conn->escape_string($_POST["konsole"])."');");
 }
 
 if($t == "get") {
