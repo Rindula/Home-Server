7ac70b6b2b211d86259982c57cb8cdff425602e2
Rindula <rindola@t-online.de>
2017-09-21 20:08:35 +0200

    Test

--- ausleihe/index.php
+++ ausleihe/index.php
@@ -13,7 +13,7 @@
     <ul>
     <?php
     $conn = new mysqli("localhost", "root", "SiSal2002", "etiketten");
-    $r = $conn->query("SELECT a.name, b.bez, a.timestamp, k.konsole FROM ausgeliehen AS a INNER JOIN beschreibung AS b ON a.id = b.id INNER JOIN konsole AS k ON b.konsole = k.id");
+    $r = $conn->query("SELECT a.name, b.bez, a.timestamp, k.konsole FROM ausgeliehen a INNER JOIN beschreibung b ON a.id = b.id INNER JOIN konsole k ON b.konsole = k.id");
     echo $conn->error;
     while ($a = $r->fetch_array()) {
         $t = date("d.m.Y", strtotime($a[2]));
