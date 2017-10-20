eeafe27c53c7223aeb8ba8dbd2b66a1e1190f380
Rindula <contact@rindula.de>
2017-10-20 16:17:40 +0200

    Changeing Scanner Again

--- scanner/list.php
+++ scanner/list.php
@@ -4,7 +4,7 @@
         <title>Liste</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <script>
             var old = "";
             var xhttp = new XMLHttpRequest();
@@ -15,16 +15,10 @@
                             document.getElementsByTagName("div")[0].innerHTML = xhttp.responseText;
                             old = xhttp.responseText;
                         }
-                        setTimeout(function() {
-                            update(document.getElementById("datepick").value);
-                            old = "";
-                        }, 1000);
                     }
                     if (this.readyState == 4 && this.status != 200) {
                         document.getElementsByTagName("div")[0].innerHTML = "<h1>Es ist ein Verbindungsfehler aufgetreten!</h1>";
-                        setTimeout(function() {
-                            update(document.getElementById("datepick").value);
-                        }, 10000);
+                        old = "";
                     }
                 };
                 xhttp.open("GET", "show.php?date=" + d, true);
@@ -32,11 +26,14 @@
             }
         </script>
     </head>
-    <body>
-        <input id="datepick" value="<?= date("Y-m") ?>" type="month" onchange="update(this.value)"><h1 class="top">Verbrauchsliste</h1>
-        <div></div>
+    <body class="bg-dark">
+        <input class="d-print-none form-control" id="datepick" value="<?= date("Y-m") ?>" type="month" onchange="update(this.value)"><h1 class="display-1 text-info d-print-none">Verbrauchsliste</h1>
+        <div class="content-fluid"></div>
     </body>
     <script>
+    setInterval(function() {
+        update(document.getElementById("datepick").value);
+    }, 1000);
     update("<?= date("Y-m") ?>");
     </script>
 </html>
