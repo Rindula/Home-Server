5cb3ecc770740145c1d9b96690ad0caaa3da58bd
Unknown <contact@rindula.de>
2017-11-12 21:58:09 +0100

    Scanner Test

--- scanner/list.php
+++ scanner/list.php
@@ -27,16 +27,28 @@
         </script>
     </head>
     <body class="bg-dark">
-        <input class="d-print-none form-control bg-dark text-light border border-warning rounded" id="datepick" value="<?= date("Y-m") ?>" type="month" onchange="update(this.value)">
+        <div class="input-append date" id="datepicker" data-date="<?= date("Y-m") ?>" data-date-format="yyyy-mm">
+            <input  type="text" readonly="readonly" name="date" >    
+            <span class="add-on"><i class="icon-th"></i></span>      
+        </div>
         <br>
         <a href="javascript:window.print()" role="button" class="btn btn-outline-info d-print-none">Drucken</a>
         <h1 class="display-1 text-info d-print-none">Verbrauchsliste</h1>
         <div class="content-fluid"></div>
     </body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    
     <script>
     setInterval(function() {
         update(document.getElementById("datepick").value);
     }, 1000);
     update("<?= date("Y-m") ?>");
+    $("#datepicker").datepicker( {
+        format: "mm-yyyy",
+        startView: "months", 
+        minViewMode: "months"
+    });
     </script>
 </html>
