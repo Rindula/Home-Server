6f6bceae55e1d2310ce639153399ab13e0b858d4
Rindula <contact@rindula.de>
2017-12-02 19:43:40 +0100

    GFS

--- gfs/query.php
+++ gfs/query.php
@@ -64,7 +64,7 @@ if($t == "searchA") {
         die();
     $r = $conn->query("SELECT b.bez, b.author, bes.code FROM bestand as b inner join beschreibung as bes on bes.id = b.id WHERE author LIKE '%$q%'");
     while ($res = $r->fetch_assoc()) {
-        echo "<a onclick=\"location.replace('?isbn=".$res["code"]."'\" class=\"list-group-item\">".$res["bez"]." (".$res["author"].")</a>";
+        echo "<a onclick=\"location.replace('?isbn=".$res["code"]."')\" class=\"list-group-item\">".$res["bez"]." (".$res["author"].")</a>";
     }
     die();
 }
