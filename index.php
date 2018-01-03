5c41d90742556024b62cfc8dc5ef04eb0e1575f8
Rindula <contact@rindula.de>
2018-01-03 23:50:26 +0100

    Index Fix 2?

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ header('Cache-Control: no-cache');
 <head>
     <title>Dateispeicher</title>
     <style id="styleing"></style>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script type="text/javascript" src="scripts.js"></script>
     <link rel="stylesheet" href="style.css">
 </head>
@@ -27,7 +28,6 @@ header('Cache-Control: no-cache');
         </div>
     </div>
 </body>
-<script type="text/javascript" src="js/popper.js"></script>
-<script type="text/javascript" src="js/bootstrap.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 
 </html>
