6d425d81b2e9a1278f4207eed3c58fad481c5c4d
Rindula <contact@rindula.de>
2018-02-20 16:28:06 +0100

    ...

--- app/api/fetch.php
+++ app/api/fetch.php
@@ -7,8 +7,8 @@ $dbpass = "SiSal2002";
 
 $mysql = new mysqli($dbhost, $dbname, $dbuser, $dbpass);
 
-$return = $mysql->query("SELECT * FROM news WHERE newsDate < adddate(now(), 14) AND newsDate >= now()");
-
+$return = $mysql->query("SELECT * FROM news");
+// WHERE newsDate < DATEADD(now(), 14) AND newsDate >= now()
 while ($r = $return->fetch_assoc()) {
     echo "<h1>".$r["title"]."</h1>";
     echo "<p>".$r["content"]."</p>";
