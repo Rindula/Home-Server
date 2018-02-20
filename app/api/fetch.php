3dc73b7b6483178bbc2c82b5ce71ec66d1d37687
Rindula <contact@rindula.de>
2018-02-20 17:15:39 +0100

    ...

--- app/api/fetch.php
+++ app/api/fetch.php
@@ -9,6 +9,6 @@ $mysql = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 $ret = $mysql->query("SELECT * FROM news WHERE newsDate BETWEEN now() AND DATE_ADD(NOW(), INTERVAL 14 DAY)");
 while ($r = $ret->fetch_assoc()) {
-    echo "<h1>".$r["title"]."</h1>";
-    echo "<p>".$r["content"]."</p>";
+    echo "<article><h1>".$r["title"]."</h1>";
+    echo "<p>".$r["content"]."</p></article>";
 }
