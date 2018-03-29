8f4fdf865abe122543f73e3ff32544d238ecbd67
Rindula <contact@rindula.de>
2018-03-29 23:42:52 +0200

    ...

--- einladungen/index.php
+++ einladungen/index.php
@@ -30,7 +30,7 @@
                                 <td>
                                     <h1>Einladung</h1>
                                     <h2>zur Geburtstagsparty</h2>
-                                    <h3>Ich werde 18</h3>
+                                    <h3 class="blend">Ich werde 18</h3>
                                 </td>
                             </tr>
                         </table>
@@ -45,22 +45,24 @@
                                         <h1>DU BIST EINGELADEN<br><?= $name ?></h1>
                                         <h2>Mitzubringen sind:</h2>
                                         <ul>
-                                            <li>Schlafsack</li>
+                                            <li>Schlafsack/Schlafzeug</li>
                                             <li>Filme (auch auf Blu-Ray)</li>
                                             <li>Taschenlampen</li>
+                                            <li>Spaß</li>
                                         </ul>
                                         <h2>Wann und wo?</h2>
                                         <p>Am <span class="important">Freitag, den 13.07.2018</span> ab <span class="important">15:00 Uhr</span> treffen wir uns bei mir (<span class="important">74889 Sinsheim, Hinterer Hettenberg 16</span>)</p>
                                         <h2>WhatsApp</h2>
-                                        <p>Scanne den Barcode um beizutreten</p>
-                                        <img style="height: 3.5cm" src="qrcode.jpg" alt="">
+                                        <p>Scanne den Barcode um der Gruppe sofort beizutreten</p>
+                                        <img style="height: 3cm" src="qrcode.png" alt="">
                                     </div>
                                 </td>
                                 <td>
                                     <h2>Was wir machen</h2>
-                                    <p>Geplant ist Kuchenessen, ein Filmabend unter freiem Himmel mit anschließender Nachtwanderung und Übernachtung. Wir können am Lagerfeuer grillen, wenn also irgendwelche Wünsche oder vorlieben/nicht-ess-wünsche gibt, bitte vorher bescheid sagen!</p>
+                                    <p>Geplant ist Kuchenessen, ein Filmabend unter freiem Himmel mit anschließender (oder vorgezogener) Nachtwanderung und Übernachtung. Wir können am Lagerfeuer grillen, wenn also irgendwelche Wünsche oder vorlieben/nicht-ess-wünsche gibt, bitte vorher bescheid sagen!</p>
                                     <p>Es können (wahrscheinlich) nicht alle übernachten, deshalb gilt: Wer den weiteren Weg hat, hat vorrang.</p>
                                     <p class="important">Als Zusage gilt, wer in der WhatsApp Gruppe ist.</p>
+                                    <p>Wenn du alkoholisches trinken möchtest, sag bitte ebenfalls bescheid, da ich nicht so viel Ahnung von dem hab, was du magst.</p>
                                 </td>
                             </tr>
                         </table>
